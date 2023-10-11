743bc309581c429f75a92ee7ce1fcdaf043217e0
sheikmohamed17 <alameen232003@gmail.com>
2023-10-11 10:15:46 +0530

    resumebuilderwithajax

---
+++ D15-20231005/resumebuilder/newrse.js
@@ -0,0 +1,579 @@
+let resumeDetails={}
+let ele_temp={}
+// function savedata(ele,detail)
+// {
+//     if(detail){
+//         if(resumeDetails[detail]){
+//             resumeDetails[detail]={...resumeDetails[detail]}
+//         }
+//         else{
+//             resumeDetails[detail]={}
+//         }
+//         resumeDetails[detail][ele.name]=ele.value;
+//     }
+//     resumeDetails[ele.name]=ele.value;
+//     console.log(resumeDetails)
+// }
+// function savedata(ele,detail){
+//     if(detail){
+//         if(resumeDetails[detail]){
+//             // resumeDetails[detail]={...resumeDetails[detail]}
+//         }
+//         else{
+//             resumeDetails[detail]={}
+//         }
+//         resumeDetails[detail][ele.name]=ele.value;
+
+//     }
+//     resumeDetails[ele.name]=ele.value
+//     console.log(resumeDetails)
+// }
+// function savedata(ele,detail)
+// {
+//     if(detail){
+//         if(resumeDetails[detail])
+//         {
+//             resumeDetails[detail]={...resumeDetails[detail]}
+//         }
+//         else{
+//             resumeDetails[detail]={}
+//         }
+//         resumeDetails[detail][ele.name]=ele.value
+//     }
+//     resumeDetails[ele.name]=ele.value
+//     console.log(resumeDetails)
+// }
+// function savedata(ele,detail){
+//     if(detail){
+//         if(resumeDetails[detail])
+//         {
+//             resumeDetails[detail]={...resumeDetails[detail]}
+//         }
+//         else{
+//             resumeDetails[detail]={}
+//         }
+//         resumeDetails[detail][ele.name]=ele.value
+//     }
+//     resumeDetails[ele.name]=ele.value
+//     console.log(resumeDetails)
+// }
+// function hwmd(p_key,ele)
+// {
+//     if(!resumeDetails[p_key]){
+//         resumeDetails[p_key]=[]
+//     }
+//      if(ele)
+//     {
+//         resumeDetails[p_key].push(document.getElementById(ele).value)
+//     }
+//     console.log(resumeDetails)
+// }
+// function hwmd(p_key,ele_id){
+//     if(!resumeDetails[p_key])
+//     {
+//      resumeDetails[p_key]=[]
+//     }
+//     if(ele_id)
+//     {
+//     resumeDetails[p_key].push(document.getElementById(ele_id).value);
+//     document.getElementById(ele_id).value='';
+//     }
+//     console.log(resumeDetails)
+// }
+
+// function savedata(ele_id,p_key){
+//     if(p_key){
+//         if(resumeDetails[p_key])
+//         {
+//             resumeDetails[p_key]={...resumeDetails[p_key]}
+//         }
+//         else
+//         {
+//            resumeDetails[p_key]={} 
+//         }
+//         resumeDetails[p_key][ele_id]=ele_id.value;
+//     }
+//     resumeDetails[ele_id.name]=ele_id.value
+//     console.log(resumeDetails)
+// }
+
+
+// function hwmd(p_key,ele){
+//     if(!resumeDetails[p_key]){
+//         resumeDetails[p_key]=[]
+//     }
+//     if(ele){
+//         resumeDetails[p_key].push(document.getElementById(ele).value)
+//     }
+//     console.log(resumeDetails)
+// }
+
+
+
+
+// function savedata(ele,detail){
+//     if(detail)
+//     {
+//     if(resumeDetails[detail])
+//         {
+//         resumeDetails[detail]={...resumeDetails[detail]}
+//         }
+//     else
+//     {
+//         resumeDetails[detail]={}
+//     }
+//     resumeDetails[detail][ele.name]=ele.value
+//     }
+//     resumeDetails[ele.name]=ele.value
+//     console.log(resumeDetails)
+   
+//     display()
+// }
+// function hwmd(p_key,ele_id)
+// {
+//     if(!resumeDetails[p_key]){
+//         resumeDetails[p_key]=[]
+//     }
+//     if(ele_id){
+//         resumeDetails[p_key].push(document.getElementById(ele_id).value)
+//     }
+//     display()
+//     console.log(resumeDetails)
+// }
+// // function disp(){
+// //     document.getElementById('showresume').innerHTML=JSON.stringify(resumeDetails)
+// // }
+// function display(){
+  
+//     document.getElementById('display_data').innerHTML=JSON.stringify(resumeDetails)
+// }
+
+// function savedata(ele,detail)
+// {
+//     console.log(ele)
+//     console.log(detail)
+//     if(detail){
+//         if(resumeDetails[detail])
+//         {
+//             resumeDetails[detail]={...resumeDetails[detail]}
+//         }
+//         else{
+//             resumeDetails[detail]={}
+//         }
+//         resumeDetails[detail][ele.name]=ele.value
+//     }
+//     resumeDetails[ele.name]=ele.value
+//     console.log(resumeDetails)
+//     disp()
+// }
+// function hwmd(p_key,ele)
+// {
+//     console.log(p_key)
+//     console.log(ele)
+//     if(!resumeDetails[p_key]){
+//         resumeDetails[p_key]=[]
+//     }
+//     if(ele)
+//     {
+//         resumeDetails[p_key].push(document.getElementById(ele).value)
+//     }
+//     if(p_key){
+
+//         resumeDetails[p_key].push(ele_temp);
+
+//     }
+//     console.log(resumeDetails)
+//     disp()
+// }
+// function disp(){
+//     document.getElementById('showresume').innerHTML=JSON.stringify(resumeDetails)
+// }
+// function hwmv(ele_v){ 
+// ele_temp[ele_v.name]=ele_v.value
+// console.log(ele_temp)
+// }
+// function savedata(ele,detail)
+// {
+//     if(detail)
+//     {
+//     if(resumeDetails[detail])
+//     {
+//         resumeDetails[detail]={...resumeDetails[detail]}
+//     }
+//     else{
+//     resumeDetails[detail]={}
+//     }
+//     resumeDetails[detail][ele.name]=ele.value
+//     }
+//     resumeDetails[ele.name]=ele.value
+//     console.log(resumeDetails)
+//     disp()
+// }
+// function hwmd(p_key,ele_id,t)
+// {
+//     if(!resumeDetails[p_key])
+//     {
+//         resumeDetails[p_key]=[]
+//     }
+//     if(ele_id)
+//     {
+//         resumeDetails[p_key].push(document.getElementById(ele_id).value)
+//         document.getElementById(ele_id).value=''
+//         // showskills(resumeDetails[p_key],ele_id)
+//         let s=resumeDetails[p_key]
+//         show(p_key,t)
+//     }
+//     else
+//     {
+//         resumeDetails[p_key].push(ele_temp)
+//         ele_id={}
+
+//     }
+//     console.log(resumeDetails)
+// //     let skil=''
+// //    for(let i=0;i<resumeDetails[p_key].length;i++)
+// //    {
+// //     let skils =resumeDetails[p_key][i]
+// //     skil=skil+`<h5>${skils}<input type='button' value='x' onclick="deletedskil('${p_key}')"></h5>`
+// //     }
+// //    document.getElementById(t).innerHTML=skil
+//     disp()
+// }
+
+// function hwmv(ele_id)
+// {
+//   ele_temp[ele_id.name]=ele_id.value 
+//   console.log(ele_temp)
+// }
+// function disp(){
+//     document.getElementById('showresume').innerHTML=JSON.stringify(resumeDetails,undefined,2)
+// }
+// // function showskills(skill,pkey)
+// // {
+// //     console.log('skill',skill)
+// //     console.log('pkey',pkey)
+// //     let show=''
+// //     for(let i=0;i<skill.length;i++)
+// //     {
+// //         let show_id=skill[i]
+// //         show=show+`'${show_id}'`
+// //         console.log('j',show)
+// //     }
+// //     document.getElementById('d').innerHTML=show
+// // }
+// function show(pid,tid)
+// {
+//     console.log(pid)
+//    let dtas='';
+//    for(let i=0;i<resumeDetails[pid].length;i++)
+//    {
+//     // let d=resumeDetails[pid][i]
+//     dtas=dtas+`<h6>${resumeDetails[pid][i]}<button onclick="dn('${pid}','${i}')">x</button></h6>`
+//    }
+//    document.getElementById(tid).innerHTML=dtas
+// }
+// function dn(indx,idnx)
+// {
+//     console.log('i',indx)
+//     console.log('id',idnx)
+//     resumeDetails[indx].splice(idnx,1)
+//     console.log(resumeDetails)
+//     disp()
+// }
+// function savedata(ele_id,p_key)
+// {
+//     if(p_key)
+//     {
+//         if(resumeDetails[p_key])
+//         {
+//             resumeDetails[p_key]={...resumeDetails[p_key]}
+//         }
+//         else{
+//             resumeDetails[p_key]={}
+//         }
+//         resumeDetails[p_key][ele_id.name]=ele_id.value
+//     }
+//     resumeDetails[ele_id.name]=ele_id.value
+//     console.log(resumeDetails)
+// }
+// function hwmd(oid,eid,dis)
+// {
+//     console.log(oid,eid)
+//     if(!resumeDetails[oid])
+//     {
+//         resumeDetails[oid]=[]
+//     }
+//     if(eid)
+//     {
+//         resumeDetails[oid].push(document.getElementById(eid).value)
+//     }
+//     console.log(resumeDetails)
+// // }
+// function savedata(ele,p_key)
+// {
+//     console.log(p_key)
+//     if(p_key)
+//     {
+//         if(resumeDetails[p_key])
+//     {
+//         resumeDetails[p_key]={...resumeDetails[p_key]}
+//     }
+//     resumeDetails[p_key][ele.name]=ele.value
+//     }
+//     resumeDetails[ele.name]=ele.value;
+//     console.log(resumeDetails)
+//     disp()
+// }
+// function hwmd(oid,eid,tid)
+// {
+//     console.log(oid,eid,tid)
+//     if(!resumeDetails[oid])
+//     {
+//         resumeDetails[oid]=[]
+//     }
+//     if(eid)
+//     {
+//         resumeDetails[oid].push(document.getElementById(eid).value)
+//    document.getElementById(eid).value='';
+
+//         disp()
+//         showsk(oid,tid)
+//     }
+//     else{
+//     resumeDetails[oid].push(ele_temp)
+//     ele_temp={}
+//     console.log(resumeDetails)
+//     showed(oid,tid)
+    
+//     disp()
+//     }
+// }
+// function showsk(pid,tbl)
+// {
+//     let ht=''
+//     for(i=0;i<resumeDetails[pid].length;i++)
+//     {
+//         a=resumeDetails[pid][i]
+//         console.log(a)
+//       ht=ht+`<h6><div id= '${pid[i]}'>${a}<button onclick="del('${pid}','${i}')">x</button></h6>`
+     
+//     }
+//     document.getElementById(tbl).innerHTML=ht
+// }
+// function disp(){
+//     document.getElementById('showresume').innerHTML=JSON.stringify(resumeDetails,undefined,2)
+// }
+// function del(id,i)
+// {
+//     console.log('k',resumeDetails[id][i])
+//     a=document.getElementById(`${id[i]}`)
+//     console.log(a)
+//     a.remove()
+//     resumeDetails[id].splice(i,1)
+//     console.log(resumeDetails)
+//     // document.getElementById(a).style.display='null';
+//     disp()
+// }
+
+
+// function hwmv(ele)
+// {
+//     console.log(ele)
+//     ele_temp[ele.name]=ele.value
+//     console.log(ele_temp)
+    
+// }
+// function showed(ed,ti)
+// {
+//     console.log(ti)
+//     a=resumeDetails[ed]
+//     console.log(';',a)
+//     data=''
+//     for(i=0;i<a.length;i++)
+//     {
+//         for(let key in a[i])
+//         {
+//            data=data+`<div id='${ed[i]}'<tr><td>'${key}'${a[i][key]}</td></tr></div>`
+//         }
+//         data=data+`<button onclick="del('${ed}','${i}')">x</button>`
+//     }
+//     document.getElementById(ti).innerHTML=data
+//     console.log(data)
+// }
+function savedata(e,o){
+    if(o)
+    {
+        if(resumeDetails[o]){
+            resumeDetails[o]={...resumeDetails}
+        }
+        resumeDetails[o]=[]
+    }
+    resumeDetails[e.name]=e.value
+    console.log(resumeDetails)
+}
+function hwmd(oi,ei,t){
+    if(!resumeDetails[oi]){
+        resumeDetails[oi]=[]
+    }
+    if(ei)
+    {
+        resumeDetails[oi].push(document.getElementById(ei).value)
+        document.getElementById(ei).value=''
+        showsk(oi,t)
+    }
+    else{
+        resumeDetails[oi].push(ele_temp)
+        ele_temp={}
+    }
+    console.log(resumeDetails)
+    disp()
+}
+function showsk(oid,ti){
+    console.log(oid,ti)
+    let d=''
+    for(i=0;i<resumeDetails[oid].length;i++)
+    {
+        a=resumeDetails[i]
+        d=d+`<div id='${oid[i]}'>${a}<button onclick=del('${oid}','${i}')>x`
+    }
+    document.getElementById(ti).innerHTML=d
+
+}
+function del(oi,i){
+    console.log(oi,i)
+    resumeDetails[oi].splice(i,1)
+    // a.splice(i,1)
+    console.log(resumeDetails[oi][i])
+}
+function hwmv(e){
+    ele_temp[e.name]=e.value
+    console.log(ele_temp)
+}
+function disp(){
+    document.getElementById('showresume').innerHTML=JSON.stringify(resumeDetails,undefined,2)
+}
+// function del(id,i)
+//       ht=ht+`<h6><div id= '${pid[i]}'>${a}<button onclick="del('${pid}','${i}')">x</button></h6>`
+// {
+//     console.log('k',resumeDetails[id][i])
+//     a=document.getElementById(`${id[i]}`)
+//     console.log(a)
+//     a.remove()
+//     resumeDetails[id].splice(i,1)
+//     console.log(resumeDetails)
+//     // document.getElementById(a).style.display='null';
+//     disp()
+// }
+
+// function hwmd(p_key,ele)
+// {
+//     if(!resumeDetails[p_key]){
+//         resumeDetails[p_key]=[]
+//     }
+//      if(ele)
+//     {
+//         resumeDetails[p_key].push(document.getElementById(ele).value)
+//     }
+//     console.log(resumeDetails)
+// }
+// function showed(dr,d)
+// {
+//     let ed=resumeDetails[d]
+//     let di=''
+//     for(i=0;i<ed.length;i++)
+//     {
+//         console.log(ed)
+//     }
+// }
+// function savedata(el,pi)
+// {
+//     console.log(el,pi)
+//     if(pi)
+//     {
+//         if(resumeDetails[pi]){
+//             resumeDetails[pi]={...resumeDetails[pi]}
+//         }
+//         else{
+//             resumeDetails[pi]={}
+//         }
+//         resumeDetails[pi][el.name]=el.value
+//     }
+//     resumeDetails[el.name]=el.value
+//     disp()
+//     console.log(resumeDetails)
+// }
+// function hwmd(oid,eid,tid)
+// {
+//     console.log(oid,eid,tid)
+//     if(!resumeDetails[oid])
+//     {
+//         resumeDetails[oid]=[]
+//     }
+//     if(eid)
+//     {
+//         resumeDetails[oid].push(document.getElementById(eid).value)
+//         showsk(oid,tid)
+//     }
+//     else{
+//         resumeDetails[oid].push(ele_temp)
+//         se(oid,tid)
+//     }
+//     console.log(resumeDetails)
+//     disp()
+// }
+//  function hwmv(ele_id)
+// {
+//   ele_temp[ele_id.name]=ele_id.value 
+//   console.log(ele_temp)
+// }
+// // function hwmv(oi)
+// // {
+// //     ele_temp[oi.id]=oi.value
+// //     console.log(ele_temp)
+// // }
+// function showsk(o,t){
+// console.log(o,t)
+// ht=''
+// for(i=0;i<resumeDetails[o].length;i++){
+//     ht=ht+`<div id='${o[i]}'>${resumeDetails[o][i]}<button onclick="del('${i}','${o}')">x</button></div>`
+// }
+// console.log(ht)
+// document.getElementById(t).innerHTML=ht
+// }
+// function del(i,o)
+// {
+    
+//     a=document.getElementById(`${o[i]}`)
+//     a.remove()
+//     resumeDetails[o].splice(i,1)
+//     disp()
+//     console.log(resumeDetails)
+// }
+// function disp(){
+//     document.getElementById('showresume').innerHTML=JSON.stringify(resumeDetails,undefined,2)
+// }
+// function se(o,t)
+// {
+//     console.log(o,t)
+//     a=resumeDetails[o]
+//     console.log(a)
+//     let dp=''
+//     for(i=0;i<a.length;i++){
+//         for(keys in a[i])
+//         {
+//             dp=dp+`<tr><td>'${keys}'${a[i][keys]}`
+//         }
+//         document.getElementById(t).innerHTML=dp
+//     }
+// }
+// function del(id,i)
+// {
+//     console.log('k',resumeDetails[id][i])
+//     a=document.getElementById(`${id[i]}`)
+//     console.log(a)
+//     a.remove()
+//     resumeDetails[id].splice(i,1)
+//     console.log(resumeDetails)
+//     // document.getElementById(a).style.display='null';
+//     disp()
+// }
--- D15-20231005/resumebuilder/resume.js
+++ D15-20231005/resumebuilder/resume.js
@@ -1,5 +1,5 @@
 let resumeDetails={};
-ele_temp={}
+let ele_temp={}
 function a(ele,detail)
 {
     if(detail)
@@ -16,29 +16,56 @@ function a(ele,detail)
     else{
         resumeDetails[ele.name]=ele.value
     }
-    console.log(resumeDetails)
+    // console.log(resumeDetails)
     display()
 }
 function handlewithmuldata(p_key,ele_id){
-   if(!resumeDetails[p_key]){
+   if(!resumeDetails[p_key])
+   {
     resumeDetails[p_key]=[]
    }
-   if(ele_id){
+   if(ele_id)
+   {
    resumeDetails[p_key].push(document.getElementById(ele_id).value);
    document.getElementById(ele_id).value='';
    }
-   else{
+   else
+   {
     resumeDetails[p_key].push(ele_temp)
     ele_temp={}
    }
    console.log(resumeDetails[p_key])
    let skil=''
-   for(let i=0;i<resumeDetails[p_key].length;i++){
+   for(let i=0;i<resumeDetails[p_key].length;i++)
+   {
     let skils =resumeDetails[p_key][i]
     skil=skil+`<h5>${skils}<input type='button' value='x' onclick="deletedskil('${p_key}')"></h5>`
- }
+    }
+    console.log(skil)
    document.getElementById('showskills').innerHTML=skil
    display()
+}
+function language(p_key,ele_id){
+    console.log(p_key)
+    console.log(ele_id)
+    let ele = document.getElementById(ele_id).value;
+
+    console.log('ele',ele);
+
+    if(!resumeDetails[p_key])
+    {
+        resumeDetails[p_key]=[]
+    }
+    
+    resumeDetails[p_key].push(document.getElementById(ele_id).value)
+     document.getElementById(ele_id).value=""
+    let lang=''
+    for(let i=0;i<resumeDetails[p_key].length;i++)
+    {
+   lang=lang+`<h5>${resumeDetails[p_key][i]}</h5>`
+    }
+    document.getElementById('showlang').innerHTML=lang
+    display()
 }
 function deletedskil(a){
     for(let i=0;i<resumeDetails['skills'].length;i++){
@@ -52,6 +79,7 @@ function deletedskil(a){
 
 function handlewithmularray(ele){
 ele_temp[ele.name]=ele.value;
+
 }
 
 // function handlewithobj(p_key){
@@ -65,3 +93,133 @@ ele_temp[ele.name]=ele.value;
 function display(){
     document.getElementById('display_data').innerHTML=JSON.stringify(resumeDetails,undefined,2)
 }
+function dataentry(){
+    $.ajax({
+        type:"POST",
+        url:"http://agaram.academy/api/action.php",
+        data:{
+            request:"create_resume",
+            user:"sheikmohamed",
+            resume:resumeDetails
+            
+        },
+        success:function(a){
+            console.log(a)
+            window.location='index.html';
+        },
+        error:function(er){
+            console.log(er)
+        },
+    })
+};
+function getdata(){
+
+    $.ajax({
+        type:"get",
+        url:"http://agaram.academy/api/action.php",
+        data:{
+            request:"get_user_resume",
+            user:"sheikmohamed"
+        },
+        success:function(a){
+            
+             console.log(a)
+            let pdata=JSON.parse(a)
+            // console.log(pdata)
+            let th=''
+            for(let i=0;i<pdata.data.length;i++)
+            {
+                // console.log(pdata.data[a].user)
+                th=th+`<tr>
+                <td>${pdata.data[i].user}</td>
+                <td>${pdata.data[i].id}</td>
+                <td> <button onclick="del('${pdata.data[i].id}')">delete</button></td>
+                </tr>`
+            }
+            document.getElementById('rdata').innerHTML=th
+        },
+        error:function(er){
+            console.log(er)
+        }
+
+    })
+};
+function del(i)
+{
+    alert(i)
+    $.ajax({
+        type:"GET",
+        url:"http://agaram.academy/api/action.php",
+
+        data:{
+            request:"delete_user_resume",
+            user:"sheikmohamed",
+            id:i
+        },
+        success:function(a){
+            console.log(a)
+           
+            getdata()
+        },
+        error:function(err){
+            console.log('del_err',err)
+        }
+    })
+}
+function getresume(){
+    $.ajax(
+        {
+            type:"GET",
+            url:"http://agaram.academy/api/action.php",
+            data:{
+                request:"get_resume_by_id",
+                user:"sheikmohamed",
+                id:8220
+            },
+            success:function(a){
+                console.log("getresume",a)
+            },
+            error:function(err){
+                console.log('grError',err)
+            }
+
+
+        }
+    )
+}
+function showtable(){
+    window.location="h.html";
+    getdata();
+}
+
+
+
+
+
+
+
+// $.ajax(
+//     {
+//         type:"GET",
+//         url:'https://jsonplaceholder.typicode.com/posts/',
+//         data:{},
+//         success:function(d){
+//             console.log('s',d);
+//             console.log('length',d.length);
+//              let hdata='';
+//             for(let i=0;i<d.length;i++)
+//             {
+//                 // console.log(`${data[i].id}`)
+//                 hdata=hdata+`<tr>
+//                 <td>${d[i].userId}</td>
+//                 <td>${d[i].id}</td>
+//                 <td><button type="button" onclick="showid('${d[i].id}')">${d[i].title}</td></button>`
+                               
+//             }
+//              document.getElementById('tbody_data').innerHTML=hdata
+//         },
+//         error:function(err){
+//             console.log("e",err)
+//         }
+//     }
+// )
